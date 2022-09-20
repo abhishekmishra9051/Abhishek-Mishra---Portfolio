4241256a50f7766485d85c512597235390c37a82
Abhishek Mishra <abhishekmishra@gmail.com>
2022-09-20 14:04:56 +0530

    loader added

--- src/containers/product/index.js
+++ src/containers/product/index.js
@@ -1,20 +1,28 @@
 import { useEffect, useState } from "react";
 import Layout from "../../hoc/layout";
-
+import Loader from '../../assets/images/loader.svg'
 //import products from "../../services/json/products.json";
 import { getProductList } from "../../services/product";
 
 export default function Product() {
   const [products, setProduct] = useState([]);
+  const [loader, setLoader] = useState(true);
+
   useEffect(() => {
     getProducts();
   }, []);
 
   const getProducts = async () => {
-    let responce = await getProductList();
-    if (responce.status === 200) {
-      setProduct(responce.data);
+    try{
+      let responce = await getProductList();
+      if (responce.status === 200) {
+        setProduct(responce.data);
+        setLoader(false)
+      }
+    }catch(e){
+      setLoader(false)
     }
+    
   };
 
   return (
@@ -25,25 +33,32 @@ export default function Product() {
     >
       <h1>New Arrivals</h1>
       <div className="row">
-        {products.map((product, key) => {
-          return (
-            <div className="col-4 mb-3">
-              <div class="card">
-                <img src={product.image} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class=" card tille">{product.productName}</h5>
-                  <p class="card-text">
-                    <strong>${product.price}</strong>
-                  </p>
-                  <p class="card-text">{product.productDesc}</p>
-                  <a href="/" class="btn btn-primary">
-                    Add to Cart
-                  </a>
+        {loader ? (
+          <div className="text-center p-5 mt-3">
+            Loading...<br />
+            <img src={Loader} alt='loader' height={50}/>
+          </div>
+        ) : (
+          products.map((product, key) => {
+            return (
+              <div className="col-4 mb-3">
+                <div class="card">
+                  <img src={product.image} class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class=" card tille">{product.productName}</h5>
+                    <p class="card-text">
+                      <strong>${product.price}</strong>
+                    </p>
+                    <p class="card-text">{product.productDesc}</p>
+                    <a href="/" class="btn btn-primary">
+                      Add to Cart
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
     </Layout>
   );
